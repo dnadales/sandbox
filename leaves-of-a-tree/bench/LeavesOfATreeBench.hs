550d541786a3fb11365e6269a267639cbe7a10c5
Bart Frenk <bart.frenk@gmail.com>
2016-05-30 15:17:57 +0200

    Evaluate to normal form for benchmarks

--- leaves-of-a-tree/bench/LeavesOfATreeBench.hs
+++ leaves-of-a-tree/bench/LeavesOfATreeBench.hs
@@ -14,11 +14,11 @@ tree1 = mkTree [0 .. 1000000]
 main :: IO ()
 main = defaultMain [
   bgroup "leaves" [
-      bench "leaves in lazy state monad tree 0" (nf leavesS tree0)
-    , bench "leaves in lazy state monad tree 1" (nf leavesS tree1)
-    , bench "leaves tree 0" $ whnf leaves tree0
-    , bench "leaves tree 1" $ whnf leaves tree1
-    , bench "leaves with continuation tree 0" $ whnf leaves' tree0
-    , bench "leaves with continuation tree 1" $ whnf leaves' tree1
+      bench "lazy state monad tree 0" (nf leavesS tree0)
+    , bench "lazy state monad tree 1" (nf leavesS tree1)
+    , bench "leaves tree 0" $ nf leaves tree0
+    , bench "leaves tree 1" $ nf leaves tree1
+    , bench "leaves with continuation tree 0" $ nf leaves' tree0
+    , bench "leaves with continuation tree 1" $ nf leaves' tree1
     ]
   ]
