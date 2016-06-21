754eb724030e5d21f753e2e5104bf341ced056f8
Damian Nadales <damian.nadales@gmail.com>
2016-06-21 10:46:32 +0200

    Added profiling support via the main program.

--- leaves-of-a-tree/README.md
+++ leaves-of-a-tree/README.md
@@ -5,4 +5,36 @@ benchmarks using stack:
 
     stack bench
 
+The benchmark program uses [Criterion](http://www.serpentine.com/criterion/) as
+benchmarking library.
+
+Alternatively, individual versions of the "leaves-of-a-tree" variants can be
+run as follows:
+
+    stack exec leaves-of-a-tree -- -v VERSION -s SIZE
+
+where `VERSION` is the string name of the variant, and `SIZE` is an integer
+specifying the size of a tree.
+
+To profile your solution make sure that a local version of `ghc` is used. The
+`ghc` version used by Stack can be seen by running:
+
+    stack exec -- which ghc
+
+If this returns a global path instead of a local path (having the prefix
+`~/.stack/`), then a global installation is used. Correct this by either
+removing the global installation or setting `system-ghc` to `false` in
+`stack.yaml` and then run:
+
+    stack setup
+
+To build the project with profiling support build the main application as
+follows:
+
+    stack build --executable-profiling --library-profiling --ghc-options="-rtsopts"
+
+Then run the main program using the `+RTS -p` flag. For instance:
+
+    stack exec leaves-of-a-tree -- -v leavesT -s 10000000 +RTS -p
+
 Is your solution faster?
--- leaves-of-a-tree/app/Main.hs
+++ leaves-of-a-tree/app/Main.hs
@@ -1,6 +1,80 @@
 module Main where
 
-import Data.String.Strip
+import           Data.BinTree
+import           Data.Either
+import qualified Data.Map.Strict     as Map
+import           Options.Applicative
+import           Text.Read
+
+data Opts = Opts
+  { version  :: String
+  , treeSize :: Integer }
+
+parseNumber :: Parser Integer
+parseNumber =
+  option auto
+  (  long  "size"
+  <> short 's'
+  <> metavar "SIZE"
+  <> help "Number of leaves of the tree")
+
+optsParser :: Parser Opts
+optsParser = Opts
+  <$> strOption
+  (  long "version"
+  <> short 'v'
+  <> help "version of the algorithm to run" )
+  <*> parseNumber
+
+
+runSafe :: Opts -> IO ()
+runSafe opts = either reportErrors run (validateOpts opts)
+
+reportErrors :: [String] -> IO ()
+reportErrors = mapM_ putStrLn
+
+validateOpts :: Opts -> Either [String] Opts
+validateOpts opts@(Opts v s) =
+  if null ls then Right opts
+  else Left ls
+  where ls = lefts [validateVersion v, validateSize s]
+
+validateVersion :: String -> Either String ()
+validateVersion v = if (v `Map.member` versions)
+                    then Right ()
+                    else Left $ v ++ " is not a version"
+
+lower :: Integer
+lower = 0
+
+upper :: Integer
+upper = 10^8
+
+validateSize :: Integer -> Either String ()
+validateSize s = if  lower <= s && s < upper
+                 then Right ()
+                 else Left $ (show s)
+                      ++ " is not a valid size. It should be in the range ["
+                      ++ (show lower)
+                      ++ ".."
+                      ++ (show upper)
+                      ++ "]"
+
+run :: Opts -> IO ()
+run (Opts v s) = putStrLn $ "sum = " ++ (show total)
+  where total = sum $ (prog) (mkTree [0 .. s])
+        prog = Map.findWithDefault (const []) v versions
+
+
+-- Versions what we will test
+versions :: Map.Map String (BinTree a -> [a])
+versions = Map.fromList
+  [ ("leaves", leaves)
+  , ("leavesT", leavesT)
+  , ("leavesComp", leaves')
+  , ("leavesS", leavesS)]
 
 main :: IO ()
-main = interact strip
+main = execParser opts >>= runSafe
+  where opts = info (helper <*> optsParser)
+               (fullDesc <> progDesc "benchmark leaves of a tree")
