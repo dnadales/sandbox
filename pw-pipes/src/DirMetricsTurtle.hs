c44779b856d4b273fa5d73d5cd76ca553cc33265
Damian Nadales <damian.nadales@gmail.com>
2017-06-15 15:42:57 +0200

    Added a more robust solution that can deal with non-utf8 `ByteString`s, but it gives a different result than:
    ```
    find test/  -type f -print | xargs cat | wc -l
    ```
    
    Since it counts one more line per file.

--- pw-pipes/src/DirMetricsTurtle.hs
+++ pw-pipes/src/DirMetricsTurtle.hs
@@ -4,10 +4,19 @@ module DirMetricsTurtle
   ( printFilesCountIn
   , printLinesCountIn
   , regularFilesIn
+  , printLinesCountIn'
+  , catDir
   ) where
 
-import qualified Control.Foldl as F
-import qualified Turtle        as T
+import qualified Control.Foldl            as F
+import           Data.ByteString          (ByteString)
+import qualified Data.ByteString          as BS
+import qualified Data.List.NonEmpty       as NE
+import qualified Data.Text                as Tx
+import           Data.Text.Encoding
+import           Data.Text.Encoding.Error
+import qualified Turtle                   as T
+import qualified Turtle.Bytes             as TB
 
 -- | Returns true iff the file path is not a symlink.
 noSymLink :: T.FilePath -> IO Bool
@@ -30,7 +39,46 @@ printFilesCountIn fPath = do
   count <- T.fold (regularFilesIn fPath) F.length
   print count
 
+-- | Read lines of `Text` from all the regular files under the given directory
+-- path.
+inputDir :: T.FilePath -> T.Shell T.Line
+inputDir fPath = do
+  file <- regularFilesIn fPath
+  T.input file
+
 -- | Print the number of lines in all the files in a directory.
 printLinesCountIn :: T.FilePath -> IO ()
-printLinesCountIn = undefined
+printLinesCountIn fPath = do
+  count <- T.fold (inputDir fPath) F.length
+  print count
 
+-- * Trying to solve the problem with non-utf8 encoding.
+
+mDecodeByteString :: T.Shell ByteString -> T.Shell T.Text
+mDecodeByteString = gMDecodeByteString (streamDecodeUtf8With lenientDecode)
+  where gMDecodeByteString :: (ByteString -> Decoding)
+                             -> T.Shell ByteString
+                             -> T.Shell T.Text
+        gMDecodeByteString f bss = do
+          bs <- bss
+          let Some res bs' g = f bs
+          if BS.null bs'
+            then return res
+            else gMDecodeByteString g bss
+
+inputDir' :: T.FilePath -> T.Shell T.Line
+inputDir' fPath = do
+  file <- regularFilesIn fPath
+  text <- mDecodeByteString (TB.input file)
+  T.select (NE.toList $ T.textToLines text)
+
+-- | Print the number of lines in all the files in a directory. Using a more
+-- robust version of `inputDir`.
+printLinesCountIn' :: T.FilePath -> IO ()
+printLinesCountIn' fPath = do
+  count <- T.fold (inputDir' fPath) T.countLines
+  print count
+
+-- -- | Cat the dir file contents.
+catDir :: T.FilePath -> IO ()
+catDir fPath = T.stdout (inputDir' fPath)
