373aca425d94121a692c12cf5b137f2309c4404c
Damian Nadales <damian.nadales@gmail.com>
2017-06-15 12:54:42 +0200

    Implemented a more elegant version of counting the number of files.

--- pw-pipes/src/DirMetricsTurtle.hs
+++ pw-pipes/src/DirMetricsTurtle.hs
@@ -3,28 +3,32 @@
 module DirMetricsTurtle
   ( printFilesCountIn
   , printLinesCountIn
+  , regularFilesIn
   ) where
 
 import qualified Control.Foldl as F
 import qualified Turtle        as T
+
+-- | Returns true iff the file path is not a symlink.
+noSymLink :: T.FilePath -> IO Bool
+noSymLink fPath = (not . T.isSymbolicLink) <$> T.stat fPath
+
+-- | Shell that outputs the regular files in the given directory.
+regularFilesIn :: T.FilePath -> T.Shell T.FilePath
+regularFilesIn fPath = do
+  fInFPath <- T.lsif noSymLink fPath
+  st <- T.stat fInFPath
+  if T.isRegularFile st
+    then return fInFPath
+    else T.empty
 
 -- | Print the number of regular files in a directory. This should implement:
 --
 -- > find $fPath -type f | wc -l
 printFilesCountIn :: T.FilePath -> IO ()
 printFilesCountIn fPath = do
-  count <- T.fold ( do
-                      fInFPath <- T.lsif noSymLink fPath
-                      st <- T.stat fInFPath
-                      if T.isRegularFile st
-                        then return 1
-                        else return 0
-                  ) (F.sum)
+  count <- T.fold (regularFilesIn fPath) F.length
   print count
-  where noSymLink f = do
-          st <- T.stat f
-          return (not (T.isSymbolicLink st))
-
 
 -- | Print the number of lines in all the files in a directory.
 printLinesCountIn :: T.FilePath -> IO ()
