adafe5a814df54bc0ab61472bf038130b0d59033
capitanbatata <damian.nadales@gmail.com>
2017-11-21 17:10:10 +0100

    Using threads and channels now, to try to reproduce the problem.

--- dangling-connections/src/Lib.hs
+++ dangling-connections/src/Lib.hs
@@ -4,6 +4,8 @@ module Lib
     ) where
 
 
+import           Control.Concurrent
+import           Control.Monad
 import           Data.String.Utils
 import           Network
 import           System.IO
@@ -12,16 +14,28 @@ someFunc :: IO ()
 someFunc = withSocketsDo $ do
     sock <- listenOn (PortNumber 9090)
     (h, _, _) <- accept sock
-    handleCmds h
+    ch <- newChan
+    readerTid <- forkIO $ handleReader h ch
+    cmdsHandler h ch
+    putStrLn "Killing the handler reader"
+    killThread readerTid
+    putStrLn "Closing the handle"
+    hClose h
 
-handleCmds :: Handle -> IO ()
-handleCmds h = do
+cmdsHandler :: Handle -> Chan Action -> IO ()
+cmdsHandler h ch = do
+    act <- readChan ch
+    case act of
+      Quit      -> putStrLn "Bye bye"
+      Line line ->  putStrLn $ "echo \"" ++ line ++ "\""
+
+handleReader :: Handle -> Chan Action -> IO ()
+handleReader h ch = forever $ do
     line <- strip <$> hGetLine h
     case line of
         "quit" -> do
-            putStrLn "Closing the handle"
-            hClose h
-            putStrLn "Bye bye"
+            writeChan ch Quit
         _ -> do
-            putStrLn $ "echo \"" ++ line ++ "\""
-            handleCmds h
+            writeChan ch (Line line)
+
+data Action = Quit | Line String
