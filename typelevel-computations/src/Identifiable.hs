48d6abd792c570794ffb53811ad7a1fc42a5adf5
capitanbatata <damian.nadales@gmail.com>
2017-12-05 15:52:31 +0100

    Added Alejandro solution. It still fails to work for nested types.

--- typelevel-computations/src/Identifiable.hs
+++ typelevel-computations/src/Identifiable.hs
@@ -4,6 +4,7 @@
 --
 {-# LANGUAGE DataKinds             #-}
 {-# LANGUAGE DefaultSignatures     #-}
+{-# LANGUAGE DeriveGeneric         #-}
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
@@ -19,63 +20,74 @@ import           GHC.Generics
 
 type Name = String
 
-newtype Id = Id { _id :: Int }
+newtype Id = Id { _id :: Int } deriving (Generic, Eq, Show)
 
-data VarId = VarId Name Id SortId
-data FuncId = FuncId Name Id [SortId] SortId
-newtype SortId = Name Id
+data VarId = VarId Name Id SortId deriving (Generic, Eq, Show)
+data FuncId = FuncId Name Id [SortId] SortId deriving (Generic, Eq, Show)
+data SortId = SortId Name Id deriving (Generic, Eq, Show)
 -- ... and 20 more of these things
 
 data Identifier = IdVar VarId
                 | IdFunc FuncId
                 | IdSort SortId
+                deriving (Generic, Eq, Show)
                 -- ... and 20 more of these things
 
-class Identifiable e where
-    getId :: e -> Id
-
-    -- And here is where everything fails: @Rep e@ has kind @* -> *@, whereas
-    -- @HasId@ expects a type (with kind :: I'd guess).
-    default getId :: (Generic e, MakeIdentifiable (HasId (Rep e)) (Rep e)) => e -> Id
-    getId = (mkGetId (Proxy :: Proxy (HasId (Rep r f)))) . from
+-- Apparently this won't work using @default@ (somehow the type @e@ in the
+-- signature is not equated to the type @e@ in the implementation).
+getId' :: forall e . (Generic e, Identifiable (HasId (Rep e)) (Rep e)) => e -> Id
+getId' x  = mkGetId (Proxy :: Proxy (HasId (Rep e))) (from x)
 
 data Crumbs = Here | GoLeft Crumbs | GoRight Crumbs
-
 data Res = Found Crumbs | NotFound
-
-type family HasId e :: Res where
-    HasId Id = Found Here
-    HasId (M1 i c a p) = HasId (a p)
-    HasId (U1 p) = NotFound
-    HasId (K1 i Id p) = Found Here
-    HasId ((l :+: r) p) = Choose (HasId (l p)) (HasId (r p))
-    HasId ((l :*: r) p) = Choose (HasId (l p)) (HasId (r p))
-    HasId a = NotFound
 
 type family Choose (a :: Res)  (b :: Res) :: Res where
     Choose (Found a) b = Found (GoLeft a)
     Choose a (Found b) = Found (GoRight b)
     Choose NotFound NotFound = NotFound
 
-class MakeIdentifiable (res :: Res) f where
+-- The kind * -> * is needed to be able to work with the representations used
+-- at Generics.
+type family HasId (e :: * -> *) :: Res where
+    HasId (M1 i c t) = HasId t
+    HasId (K1 i Id) = Found Here
+    HasId (l :+: r) = Choose (HasId l) (HasId r)
+    HasId (l :*: r) = Choose (HasId l) (HasId r)
+    HasId a = NotFound
+
+class Identifiable (res :: Res) f where
     mkGetId :: Proxy res -> f e -> Id
 
-instance MakeIdentifiable (Found Here) (K1 i Id) where
+instance Identifiable (Found Here) (K1 i Id) where
     mkGetId _ (K1 x) = x
 
-instance MakeIdentifiable (Found c) a => MakeIdentifiable (Found (GoLeft c)) (a :+: b) where
+instance Identifiable (Found c) a => Identifiable (Found (GoLeft c)) (a :+: b) where
     -- ScopedTypeVariables needed for having the @c@ in the implementation
     -- refer to the same @c@ in the instance constraints.
     mkGetId _ (L1 a) = mkGetId (Proxy :: Proxy (Found c)) a
 
-instance MakeIdentifiable (Found c) b => MakeIdentifiable (Found (GoRight c)) (a :+: b) where
+instance Identifiable (Found c) b => Identifiable (Found (GoRight c)) (a :+: b) where
     mkGetId _ (R1 b) = mkGetId (Proxy :: Proxy (Found c)) b
 
-instance MakeIdentifiable (Found c) a => MakeIdentifiable (Found (GoLeft c)) (a :*: b) where
+instance Identifiable (Found c) a => Identifiable (Found (GoLeft c)) (a :*: b) where
     mkGetId _ (a :*: b) = mkGetId (Proxy :: Proxy (Found c)) a
 
-instance MakeIdentifiable (Found c) b => MakeIdentifiable (Found (GoRight c)) (a :*: b) where
+instance Identifiable (Found c) b => Identifiable (Found (GoRight c)) (a :*: b) where
     mkGetId _ (a :*: b) = mkGetId (Proxy :: Proxy (Found c)) b
 
-instance MakeIdentifiable (Found c) a => MakeIdentifiable (Found c) (M1 i d a) where
+instance Identifiable (Found c) a => Identifiable (Found c) (M1 i d a) where
     mkGetId _ (M1 x) = mkGetId (Proxy :: Proxy (Found c)) x
+
+-- | Problem:
+--
+-- This works:
+--
+-- λ> getId' (SortId "hello" (Id 10))
+-- > Id {_id = 10}
+--
+-- Bot this won't:
+--
+-- λ> getId' (IdSort (SortId "hello" (Id 10)))
+--
+--
+-- > No instance for (Identifiable 'NotFound ...)
