b9d04f77956306657d67e82c66ec3513d4b4a5f9
capitanbatata <damian.nadales@gmail.com>
2017-12-28 13:59:49 +0100

    - Added more symmetry to the server and client processes. - Made everything more robust.

--- receiving-strings-via-sockets/test/Network/TextViaSocketsSpec.hs
+++ receiving-strings-via-sockets/test/Network/TextViaSocketsSpec.hs
@@ -10,6 +10,7 @@ import Control.Exception.Base hiding (assert)
 import Control.Concurrent.Async
 import qualified Data.Text as T
 import Control.Arrow
+import Control.Concurrent.MVar
 
 import           Test.Hspec
 import           Test.QuickCheck
@@ -23,38 +24,66 @@ newtype ValidPort = ValidPort {port :: PortNumber} deriving (Show, Eq)
 
 instance Arbitrary ValidPort where
     arbitrary = do
-        nr <- choose (9000, 65535)
+        nr <- choose (49152, 65535) -- See https://en.wikipedia.org/wiki/Ephemeral_port
         return $ ValidPort (fromInteger nr)
 
 -- | Timeout token.
 data Timeout = Timeout
 
-readerProcess :: IO Connection -> Int -> IO (Either Timeout [Text])
-readerProcess mConn howMany =
-    left (const Timeout) <$> timeout `race` doRead
-    where doRead = do
-              conn <- mConn
-              lines <- replicateM howMany (readLineFrom conn)
+cliProc :: ValidPort -- ^ Port to connect to. 
+        -> Int       -- ^ Number of messages to be received.
+        -> [Text]    -- ^ Messages to send to the server.
+        -> MVar (Async a) -- ^ Async handle to the server process. If an
+                          -- exception arises at the client, the server process
+                          -- has to be canceled.
+        -> IO (Either Timeout [Text])
+cliProc vPort howMany svrMsgs aSvrTV =
+    timeout `race` (cliProc' `catch` handler)
+    where cliProc' = do
+              conn <- connectTo "localhost" (show (port vPort))
+              a <- async $ sendMsgs conn svrMsgs
+              res <- receiveMsgs conn howMany
+              wait a
               close conn
-              return lines
-    
-writerProcess :: IO Connection
-              -> [Text]
-              -> Async a -- ^ Async handle to the reader process. If an
-                         -- exception arises at the writer, this process has to
-                         -- be canceled.
-              -> IO ()
-writerProcess mConn msgs a = 
-    (tryToWrite `catch` handler) `race_` timeout
+              return res
+    -- An IOException will likely happen when we the
+    -- address is already in use.        
+          handler :: IOException -> IO [Text]
+          handler ex = do
+              aSvr <- takeMVar aSvrTV
+              cancel aSvr
+              throwIO ex  
+              
+
+sendMsgs :: Connection -> [Text] -> IO ()
+sendMsgs conn msgs = traverse_ (putLineTo conn) msgs
+
+receiveMsgs :: Connection -> Int -> IO [Text]
+receiveMsgs conn howMany = replicateM howMany (readLineFrom conn)
+
+svrProc :: ValidPort -- ^ Port to serve on.
+        -> Int       -- ^ Number of messages to be received.
+        -> [Text]    -- ^ Messages to send to the client.
+        -> MVar (Async a) -- ^ Async handle to the client process. If an exception
+                          -- arises at the server, this process has to be canceled.
+        -> IO (Either Timeout [Text])
+svrProc vPort howMany msgs aCliTV = do
+    timeout `race` (svrProc' `catch` handler)
   where
-    tryToWrite = do
-        conn <- mConn
-        traverse_ (putLineTo conn) msgs
+    svrProc' = do
+        conn <- acceptOn (port vPort)
+        a <- async $ receiveMsgs conn howMany
+        sendMsgs conn msgs
+        res <- wait a
         close conn
-        return ()
-    handler :: IOException -> IO ()
-    handler ex = cancel a -- An IOException will likely happen when we the
-                          -- address is already in use.
+        return res
+    -- An IOException will likely happen when we the
+    -- address is already in use.        
+    handler :: IOException -> IO [Text]
+    handler ex = do
+        aCli <- takeMVar aCliTV
+        cancel aCli
+        throwIO ex  
 
 checkMessages :: Either a (Either Timeout [Text]) -> [Text] -> PropertyM IO ()
 checkMessages (Left _) _ = monitor $ collect "Address in use."
@@ -69,37 +98,38 @@ checkMessages (Right (Right lines)) expected =
         run $ print $ expected
         assert False
 
-allMessagesReceived :: IO Connection -- ^ How to get a reader connection.
-                    -> IO Connection -- ^ How to get a writer connection.
-                    -> ValidPort
-                    -> [PrintableString]
+allMessagesReceived :: ValidPort         -- ^ A port where 
+                    -> [PrintableString] -- ^ Messages to be sent to the client.
+                    -> [PrintableString] -- ^ Messages to be sent to the server.
                     -> Property
-allMessagesReceived readerConnM writerConnM validPort strs = monadicIO $ do
-    a <- run $ async $ readerProcess readerConnM (length msgs)
-    run $ forkIO $ writerProcess writerConnM msgs a
-    res <- run $ waitCatch a
-    checkMessages res msgs
+allMessagesReceived port strsCli strsSvr = monadicIO $ do
+    aCliTV <- run $ newEmptyMVar
+    aSvrTV <- run $ newEmptyMVar    
+    aCli <- run $ async $ cliProc port (length msgsCli) msgsSvr aSvrTV
+    aSvr <- run $ async $ svrProc port (length msgsSvr) msgsCli aCliTV
+    run $ putMVar aCliTV aCli
+    run $ putMVar aSvrTV aSvr    
+    resCli <- run $ waitCatch aCli
+    resSvr <- run $ waitCatch aSvr
+    checkMessages resCli msgsCli
+    checkMessages resSvr msgsSvr
     where
-      msgs = map (T.pack . getPrintableString) strs
+      msgsCli = map (T.pack . getPrintableString) strsCli
+      msgsSvr = map (T.pack . getPrintableString) strsSvr
 
 clientReceivesAll :: ValidPort -> [PrintableString] -> Property
-clientReceivesAll validPort strs =
-    allMessagesReceived connectToServer serveOnPort validPort strs
-    where
-      connectToServer = connectTo "localhost" (show (port validPort))
-      serveOnPort = acceptOn (port validPort)
+clientReceivesAll vPort strs =
+    allMessagesReceived vPort strs []
 
-timeout :: IO ()
-timeout =
-    threadDelay (10^6)
-
+timeout :: IO Timeout
+timeout = do
+    threadDelay (2 * 10^6)
+    putStrLn "timing out"
+    return Timeout
 
 serverReveivesAll :: ValidPort -> [PrintableString] -> Property
-serverReveivesAll validPort strs =
-    allMessagesReceived serveOnPort connectToServer validPort strs
-    where
-      connectToServer = connectTo "localhost" (show (port validPort))
-      serveOnPort = acceptOn (port validPort)
+serverReveivesAll vPort strs =
+    allMessagesReceived vPort [] strs
 
 serverAndClientReceiveAll :: ValidPort
                           -> [PrintableString] -- ^ Messages for the server.
