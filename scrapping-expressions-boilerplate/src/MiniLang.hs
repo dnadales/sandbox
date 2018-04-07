c77a46c5faa1412efc5541438a1283b05e348d0c
capitanbatata <damian.nadales@gmail.com>
2018-04-07 23:29:03 +0200

    Add another examples of unisng lenses and `uniplate` and `biplate`.

---
+++ scrapping-expressions-boilerplate/src/MiniLang.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+module MiniLang where
+
+import           Control.Lens.Fold   ((^..))
+import           Control.Lens.Setter (over)
+import           Data.Data
+import           Data.Data.Lens      (biplate, uniplate)
+
+data Tree a = Leaf a
+            | Fork (Tree a) (Tree a)
+            deriving (Data, Show, Eq)
+
+t0 = Fork (Fork (Leaf "foo") (Leaf "bar")) (Leaf "baz")
+
+t1 = Fork t0 t0
+
+t2 = Fork (Fork t0 t0) (Fork (Leaf "zero") (Leaf "one"))
+
+reverseT :: Data a => Tree [a] -> Tree [a]
+reverseT (Leaf xs) = Leaf (reverse xs)
+reverseT xt        = over uniplate reverseT xt
+
+leavesT :: Data a => Tree a -> [a]
+leavesT xt       = xt ^.. biplate
