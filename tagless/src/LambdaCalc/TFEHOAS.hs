81ba869d9d5ae9bb97533640aecc605061eb4bf5
Damian Nadales <damian.nadales@gmail.com>
2018-07-26 13:49:56 +0200

    Extends the interpreter with multiplication, booleans, and a fixpoint operation.

--- tagless/src/LambdaCalc/TFEHOAS.hs
+++ tagless/src/LambdaCalc/TFEHOAS.hs
@@ -1,6 +1,8 @@
 -- | Tagless final embedding with higher-order abstract syntax.
 module LambdaCalc.TFEHOAS where
 
+import qualified Data.Function as F
+
 class Symantics repr where
     int :: Int -> repr Int
     add :: repr Int -> repr Int -> repr Int
@@ -78,3 +80,69 @@ instance Symantics S where
 --
 view :: S a -> String
 view e = unS e 0
+
+--------------------------------------------------------------------------------
+-- Extending the interpreter
+--------------------------------------------------------------------------------
+
+class MulSYM repr where
+    mul :: repr Int -> repr Int -> repr Int
+
+class BoolSYM repr where
+    bool :: Bool -> repr Bool
+    leq :: repr Int -> repr Int -> repr Bool
+    if_ :: repr Bool -> repr a -> repr a -> repr a
+
+class FixSYM repr where
+    fix :: (repr a -> repr a) -> repr a
+
+-- | Power function defined in this language:
+tpow :: (Symantics repr, MulSYM repr, BoolSYM repr, FixSYM repr)
+     => repr (Int -> Int -> Int)
+tpow = lam (\x -> fix (\self -> lam (\n ->
+                                         if_ (leq n (int 0))
+                                             (int 1)
+                                             (mul x (app self (add n (int (-1)))))
+                                         )
+                      )
+           )
+
+tpow7 :: (Symantics repr, MulSYM repr, BoolSYM repr, FixSYM repr)
+      => repr (Int -> Int)
+tpow7 = lam (\x -> (tpow `app` x) `app` int 7)
+
+tpow72 :: (Symantics repr, MulSYM repr, BoolSYM repr, FixSYM repr)
+       => repr Int
+tpow72 = app tpow7 (int 2)
+
+instance MulSYM R where
+    mul e0 e1 = R $ unR e0 * unR e1
+
+instance BoolSYM R where
+    bool b = R b
+    leq e0 e1 = R $ unR e0 <= unR e1
+    if_ g e0 e1 = R $ if unR g then unR e0 else unR e1
+
+instance FixSYM R where
+    fix fr = R $ F.fix (\a -> unR . fr . R $ a)
+
+--------------------------------------------------------------------------------
+-- Extending the pretty printer.
+--------------------------------------------------------------------------------
+
+instance MulSYM S where
+    mul e0 e1 = S $ \i -> unS e0 i ++ " * " ++ unS e1 i
+
+instance BoolSYM S where
+    bool = S . const . show
+
+    leq e0 e1 = S $ \i -> unS e0 i ++ " <= " ++ unS e1 i
+
+    if_ g e0 e1 = S $ \i ->
+        "(if " ++ unS g i ++ " then " ++ unS e0 i ++ " else " ++ unS e1 i ++ ")"
+
+instance FixSYM S where
+    fix fr = S $ \i ->
+        let self = "self" ++ show i in
+            "(fix " ++ self ++ " . "++  unS (fr (S $ const self)) (i + 1) ++ ")"
+
