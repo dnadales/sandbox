1f9dc8934af7e8ba62797b182c83b5392761b045
Damian Nadales <damian.nadales@gmail.com>
2018-08-14 16:36:49 +0200

    Complete the encoding of UTxO based ledgers.

--- chimeric-ledgers/src/ChimericLedgers/Ledger.hs
+++ chimeric-ledgers/src/ChimericLedgers/Ledger.hs
@@ -1,14 +1,23 @@
+{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE TypeFamilies    #-}
 module ChimericLedgers.Ledger where
 
-import           Data.List                   (foldl')
+import           Data.List                   (find, foldl')
+import           Data.Maybe                  (mapMaybe)
 import           Data.Set                    (Set)
 import qualified Data.Set                    as Set
+import           GHC.Exts                    (IsList, Item, fromList, toList)
+import           Safe                        (atMay)
 
-import           ChimericLedgers.Id
-import           ChimericLedgers.Transaction hiding (unspentOutputs)
+import           ChimericLedgers.Transaction hiding (value)
 import qualified ChimericLedgers.Transaction as T
 
-newtype Ledger = Ledger [Transaction]
+newtype Ledger t = Ledger [t]
+
+instance IsList (Ledger t) where
+    type Item (Ledger t) = t
+    fromList = Ledger
+    toList (Ledger ts) = ts
 
 --------------------------------------------------------------------------------
 -- Functions on input, ledgers, and transactions.
@@ -19,28 +28,69 @@ newtype Ledger = Ledger [Transaction]
 -- > hash t = tid i
 --
 -- See also 'tid'.
-tx :: Input -> Ledger -> Maybe UtxoTx
-tx = undefined
+tx :: Input -> Ledger UtxoTx -> Maybe UtxoTx
+tx i (Ledger ts) = find ((tid i ==) . hash) ts
 
 -- | Retrieves the output referred by the given input.
 --
 -- If @tx i = Just t@, then
 --
--- > out i = Just $ inputs t !! index i
-out :: Input -> Ledger -> Maybe Output
-out = undefined
+-- > out i = Just $ outputs t !! index i
+--
+out :: Input -> Ledger UtxoTx -> Maybe Output
+out i λ = do
+    t <- tx i λ
+    outputs t `atMay` index i
 
--- | Retrieves the value associated to the given output.
+-- | Retrieves the value associated to the given input.
 --
 -- If @out i = Just o@, then
 --
 -- > value i = Just $ value o
 --
-value :: Input -> Ledger -> Maybe Value
-value = undefined
+value :: Ledger UtxoTx -> Input -> Maybe Value
+value λ i = T.value <$> out i λ
 
 -- | Calculate the unspent outputs in a ledger.
-unspentOutputs :: Ledger -> Set Input
-unspentOutputs (Ledger xs) = foldl' update Set.empty  xs
-    where update :: Set Input -> Transaction -> Set Input
-          update = undefined
+unspentOutputs :: Ledger UtxoTx -> Set Input
+unspentOutputs (Ledger ts) = foldl' update Set.empty ts
+    where update :: Set Input -> UtxoTx -> Set Input
+          update acc t = (acc `Set.difference` T.spentOuts t)
+                         `Set.union` T.unspentOuts t
+
+-- | Is a transaction @t@ valid w.r.t a ledger @λ@?
+isValid :: Ledger UtxoTx -> UtxoTx -> Bool
+isValid λ t = allInputsUnspent && valueIsPreserved
+    where
+      allInputsUnspent = inputs t `Set.isSubsetOf` unspentOutputs λ
+      valueIsPreserved = forge t + sum inputValues == fee t + sum outputValues
+      inputValues = mapMaybe (value λ) (inputsList t)
+      outputValues = T.value <$> outputs t
+
+-- | Balance of an address @a@ in a valid transaction @t@, w.r.t. a given
+-- ledger @λ@.
+βUTxO :: Address -> Ledger UtxoTx -> UtxoTx -> Value
+βUTxO a λ@(Ledger ts) t = paidOutputs - spentOutputs
+    where paidOutputs :: Value -- TODO: what should happen if @paidOutputs < spentOutputs@?
+          paidOutputs = sumValues $ paidToA $ outputs t
+          spentOutputs = sumValues $ paidToA $ mapMaybe (`out` λ) (inputsList t)
+          sumValues = sum . map T.value
+          paidToA = filter ((a ==) . address)
+
+-- | Balance of an address @a@ in a given ledger @λ@.
+--
+-- Since this function and 'βUTxO' have different number of arguments, I cannot
+-- get away with defining them in a type-class (which might not be the best
+-- modelling decision anyway). If we want to use the same name, we have to
+-- define these operations in separate modules.
+βUTxOL :: Address -> Ledger UtxoTx -> Value
+βUTxOL a λ@(Ledger ts) = foldl' (+) 0 $ map (βUTxO a λ) ts
+
+--------------------------------------------------------------------------------
+-- Examples (add them as tests later)
+--------------------------------------------------------------------------------
+l0 :: Ledger UtxoTx
+l0 = [t1, t2, t3, t4, t5, t6]
+
+-- TODO: add this as a test.
+res = unspentOutputs l0 == [t6 @@ 0]
--- chimeric-ledgers/src/ChimericLedgers/Transaction.hs
+++ chimeric-ledgers/src/ChimericLedgers/Transaction.hs
@@ -1,19 +1,30 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedLists            #-}
+{-# LANGUAGE TypeOperators              #-}
 module ChimericLedgers.Transaction where
 
-import           Data.Set           (Set)
-import qualified Data.Set           as Set
-import           Data.Word          (Word32)
-
-import           ChimericLedgers.Id
-
-data Transaction
+import           Data.Set  (Set)
+import qualified Data.Set  as Set
+import           Data.Word (Word32)
 
 data UtxoTx = UtxoTx
     { inputs  :: Set Input
     , outputs :: [Output]
     , forge   :: Value
     , fee     :: Value
-    }
+    } deriving (Eq, Ord, Show)
+
+newtype Id = Id UtxoTx
+    deriving (Eq, Ord, Show)
+
+-- | @hash c@ is (should be) the cryptographic collision-resistant hash of
+-- object @c@.
+--
+hash :: UtxoTx -> Id
+hash = Id
+
+inputsList :: UtxoTx -> [Input]
+inputsList = Set.toList . inputs
 
 data Input = Input
     { -- | Id of the previous transaction to which this input refers.
@@ -23,22 +34,91 @@ data Input = Input
     , index :: Int
     } deriving (Eq, Ord, Show)
 
+(@@) :: UtxoTx -> Int -> Input
+t @@ i = Input (hash t) i
+
 data Output = Output
     { -- | Address that owns the value.
       address :: Address
     , value   :: Value
-    }
+    } deriving (Eq, Ord, Show)
 
-type Value = Word32
+($-->@) :: Value -> Address -> Output
+v $-->@ a = Output a v
 
-type Address = Word32
+newtype Value = Value Word32
+    deriving (Eq, Ord, Num, Show)
+
+newtype Address = Address Word32
+    deriving (Eq, Ord, Num, Show)
 
 -- | Calculate the unspent outputs, by calculating a set of inputs that hold a
 -- reference to the unspent transactions.
 unspentOuts :: UtxoTx -> Set Input
-unspentOuts t = Set.fromList $ fmap (uncurry Input) $ zip outHashes [0..]
-    where outHashes = hash <$> outputs t
+unspentOuts t = Set.fromList $ fmap (uncurry Input) $ zip tHashes [0..]
+    where tHashes = replicate (length (outputs t)) (hash t)
 
 -- | Outputs spent by a transaction. This is just the set of inputs.
 spentOuts :: UtxoTx -> Set Input
 spentOuts = inputs
+
+--------------------------------------------------------------------------------
+-- Examples
+--------------------------------------------------------------------------------
+
+t1 :: UtxoTx
+t1 = UtxoTx
+    { inputs = []
+    , outputs = [1000 $-->@ 1]
+    , forge = 1000
+    , fee = 0
+    }
+
+t2 :: UtxoTx
+t2 = UtxoTx
+    { inputs = [t1 @@ 0]
+    , outputs = [800 $-->@ 2, 200 $-->@ 1]
+    , forge = 0
+    , fee = 0
+    }
+
+t3 :: UtxoTx
+t3 = UtxoTx
+    { inputs = [t2 @@ 1]
+    , outputs = [199 $-->@ 3]
+    , forge = 0
+    , fee = 1
+    }
+
+
+t4 :: UtxoTx
+t4 = UtxoTx
+    { inputs = [t3 @@ 0]
+    , outputs = [207 $-->@ 2] -- The total amount we can spend is: 199 + 10 - 2
+    , forge = 10 -- On this model, the money that is forged can be spent on any output.
+    , fee = 2
+    }
+
+
+t5 :: UtxoTx
+t5 = UtxoTx
+    { inputs = [t4 @@ 0, t2 @@ 0]
+    , outputs = [500 $-->@ 2, 500 $-->@ 3]
+    , forge = 0
+    , fee = 7
+    }
+
+t6 :: UtxoTx
+t6 = UtxoTx
+    { inputs = [t5 @@ 0, t5 @@ 1]
+    , outputs = [999 $-->@ 3]
+    , forge = 0
+    , fee = 1
+    }
+
+-- TODO:
+-- Add some tests
+-- ghci> isValid [t1, t2, t3, t4, t5] t6
+--
+-- [t1, t2, t3, t4, t5 t6]
+
