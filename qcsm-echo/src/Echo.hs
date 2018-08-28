7e2c909e02d2b526047eba0302870b1cd4007dd2
Damian Nadales <damian.nadales@gmail.com>
2018-08-28 15:29:06 +0200

    Add the first working version of the Echo SUT and tests.

--- qcsm-echo/src/Echo.hs
+++ qcsm-echo/src/Echo.hs
@@ -1,5 +1,30 @@
 -- | Echo API.
 
-module Echo where
+module Echo (Env, input, mkEnv, output) where
 
--- 
+import           Control.Concurrent.STM      (atomically)
+import           Control.Concurrent.STM.TVar (TVar, newTVarIO, readTVar,
+                                              writeTVar)
+
+data Env = Env
+    { _buf :: TVar (Maybe String) }
+
+-- | Create a new environment.
+mkEnv :: IO Env
+mkEnv = Env <$> newTVarIO Nothing
+
+-- | Input a string. Returns 'True' iff the buffer was empty and the given
+-- string was added to it.
+input :: Env -> String -> IO Bool
+input (Env mBuf) str = atomically $ do
+    res <- readTVar mBuf
+    case res of
+        Nothing -> writeTVar mBuf (Just str) >> return True
+        Just _  -> return False
+
+-- | Output the buffer contents.
+output :: Env -> IO (Maybe String)
+output (Env mBuf) = atomically $ do
+    res <- readTVar mBuf
+    writeTVar mBuf Nothing
+    return res
--- qcsm-echo/test/EchoSpec.hs
+++ qcsm-echo/test/EchoSpec.hs
@@ -12,37 +12,130 @@ import           GHC.Generics                  (Generic, Generic1)
 
 import           Data.TreeDiff                 (ToExpr)
 import           Test.Hspec                    (Spec, it, pending)
-import           Test.QuickCheck               (Property, (===))
+import           Test.QuickCheck               (Gen, Property, arbitrary,
+                                                ioProperty, oneof, (===))
 import           Test.QuickCheck.Monadic       (monadicIO)
-import           Test.StateMachine             (Concrete, Reason (Ok),
-                                                StateMachine, checkCommandNames,
+import           Test.StateMachine             (Concrete, GenSym,
+                                                Logic (Bot, Top), Reason (Ok),
+                                                StateMachine (StateMachine),
+                                                Symbolic, checkCommandNames,
                                                 forAllCommands, prettyCommands,
-                                                runCommands)
+                                                runCommands, (.==))
+import           Test.StateMachine.Types       (distribution, generator,
+                                                initModel, invariant, mock,
+                                                postcondition, precondition,
+                                                runner, semantics, shrinker,
+                                                spostcondition, transition)
 import qualified Test.StateMachine.Types.Rank2 as Rank2
 
+import           Echo                          (Env, input, mkEnv, output)
+
 spec :: Spec
-spec = it "Right echo implementation" pending
+spec = it "Right echo implementation" $ ioProperty $ do
+    env <- mkEnv
+    return (prop_echoOK env)
 
-prop_echoOK :: Property
-prop_echoOK = forAllCommands echoSM Nothing $ \cmds -> monadicIO $ do
-    (hist, _, res) <- runCommands echoSM cmds
-    prettyCommands echoSM hist (res === Ok)
+prop_echoOK :: Env -> Property
+prop_echoOK env = forAllCommands echoSM' Nothing $ \cmds -> monadicIO $ do
+    (hist, _, res) <- runCommands echoSM' cmds
+    prettyCommands echoSM' hist (res === Ok)
+    where echoSM' = echoSM env
 
-echoSM :: StateMachine Model Action IO Response
-echoSM = undefined
+echoSM :: Env -> StateMachine Model Action IO Response
+echoSM env = StateMachine
+    { initModel = Empty
+    -- ^ At the beginning of time nothing was received.
+    , transition = mTransitions
+    , precondition = mPreconditions
+    , postcondition = mPostconditions
+    , spostcondition = Nothing
+    , generator = mGenerator
+    , invariant = Nothing
+    , distribution = Nothing
+    , shrinker = mShrinker
+    , semantics = mSemantics
+    , mock = mMock
+    , runner = id
+    }
+    where
+      mTransitions :: Model r -> Action r -> Response r -> Model r
+      mTransitions Empty   (In str) _   = Buf str
+      mTransitions (Buf _) Echo     _   = Empty
+      mTransitions Empty   Echo     _   = Empty
+      -- TODO: qcsm will match the case below. However we don't expect this to happen!
+      mTransitions (Buf str) (In _)   _ = Buf str -- Dummy response
+          -- error "This shouldn't happen: input transition with full buffer"
+
+      -- | There are no preconditions for this model.
+      mPreconditions :: Model Symbolic -> Action Symbolic -> Logic
+      mPreconditions _ _ = Top
+
+      -- | Post conditions for the system.
+      mPostconditions :: Model Concrete -> Action Concrete -> Response Concrete -> Logic
+      mPostconditions Empty     (In _) InAck     = Top
+      mPostconditions (Buf _)   (In _) ErrFull   = Top
+      mPostconditions _         (In _) _         = Bot
+      mPostconditions Empty     Echo   ErrEmpty  = Top
+      mPostconditions Empty     Echo   _         = Bot
+      mPostconditions (Buf str) Echo   (Out out) = str .== out
+      mPostconditions (Buf _)   Echo   _         = Bot
+
+      -- | Generator for symbolic actions.
+      mGenerator :: Model Symbolic -> Gen (Action Symbolic)
+      mGenerator _ =  oneof
+          [ In <$> arbitrary
+          , return Echo
+          ]
+
+      -- | Trivial shrinker.
+      mShrinker :: Action Symbolic -> [Action Symbolic]
+      mShrinker _ = []
+
+      -- | Here we'd do the dispatch to the actual SUT.
+      mSemantics :: Action Concrete -> IO (Response Concrete)
+      mSemantics (In str) = do
+          success <- input env str
+          return $ if success
+                   then InAck
+                   else ErrFull
+      mSemantics Echo = maybe ErrEmpty Out <$> output env
+
+      -- | What is the mock for?
+      mMock :: Model Symbolic -> Action Symbolic -> GenSym (Response Symbolic)
+      mMock Empty (In _)   = return InAck
+      mMock (Buf _) (In _) = return ErrFull
+      mMock Empty Echo     = return ErrEmpty
+      mMock (Buf str) Echo = return (Out str)
 
 deriving instance ToExpr (Model Concrete)
 
--- | The model holds the last string that was input.
-newtype Model (r :: * -> *) = Model (Maybe String)
+-- | The model contains the last string that was communicated in an input
+-- action.
+data Model (r :: * -> *)
+    = -- | The model hasn't been initialized.
+      Empty
+    | -- | Last input string (a buffer with size one).
+      Buf String
   deriving (Eq, Show, Generic)
 
--- | The system supports only a single type of input actions, which contain the
--- string to be echoed.
-data Action (r :: * -> *) = In String
+-- | Actions supported by the system.
+data Action (r :: * -> *)
+    = -- | Input a string, which should be echoed later.
+      In String
+      -- | Request a string output.
+    | Echo
   deriving (Show, Generic1, Rank2.Foldable, Rank2.Traversable, Rank2.Functor)
 
 -- | The system gives a single type of output response, containing a string
 -- with the input previously received.
-data Response (r :: * -> *) = Out String
+data Response (r :: * -> *)
+    = -- | Input acknowledgment.
+      InAck
+      -- | The previous action wasn't an input, so there is no input to echo.
+      -- This is: the buffer is empty.
+    | ErrEmpty
+      -- | There is already a string in the buffer.
+    | ErrFull
+      -- | Output string.
+    | Out String
   deriving (Show, Generic1, Rank2.Foldable, Rank2.Traversable, Rank2.Functor)
