4b19a41c91a59d7a951fe89096b73f2b2ec795bf
Damian Nadales <damian.nadales@iohk.io>
2020-02-19 17:44:44 +0100

    Use `serializeEncoding'`

--- lookups-memory-usage/src/Main.hs
+++ lookups-memory-usage/src/Main.hs
@@ -11,7 +11,7 @@ import           Data.ByteString (ByteString)
 import qualified Data.ByteArray as BA
 import           Data.Serialize (encode)
 
-import           Cardano.Binary (serialize')
+import           Cardano.Binary (serialize', serializeEncoding', toCBOR)
 import           Cardano.Prelude (noUnexpectedThunks)
 
 main :: IO ()
@@ -22,7 +22,7 @@ main = do
   where
     n = 10^6
     theHashOf :: Int -> ByteString
-    theHashOf = encode
+    theHashOf i = serializeEncoding' (toCBOR i) -- encode
     result = Map.lookup (theHashOf n) stakeDist
     stakeDist :: Map ByteString Int
     !stakeDist = Map.fromList
