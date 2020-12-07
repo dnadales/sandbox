aa08276b29341c5a3fe963960836aeb6da2368e5
Damian Nadales <damian.nadales@iohk.io>
2020-12-07 19:36:38 +0100

    Add a `format` function to the `HasPrintf` class, and define instances for it.

--- thinking-with-types/src/Chapter9/Printf.hs
+++ thinking-with-types/src/Chapter9/Printf.hs
@@ -4,11 +4,15 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Chapter9.Printf where
 
 import Data.Kind (Type)
-import GHC.TypeLits (Symbol)
+import GHC.TypeLits (Symbol, symbolVal, KnownSymbol)
+import Data.Proxy (Proxy(Proxy))
+import Data.Monoid ((<>))
 
 -- Note the absence of a data constructor.
 --
@@ -20,13 +24,25 @@ infixr 5 :<<
 
 class HasPrintf a where
   type Printf a :: Type
+  format :: String
+         -- ^ This is an implementation detail that acts as an accumulator
+         -- where we can keep track of all of the formatting done by earlier
+         -- steps.
+         -> Proxy a
+         -- ^ This parameter exists only to allow GHC to find the correct
+         -- instance of 'HasPrintf' from the call site of 'format'.
+         -> Printf a
 
 -- Typelevel symbol.
-instance HasPrintf (text :: Symbol) where
+instance KnownSymbol text => HasPrintf (text :: Symbol) where
   type Printf text = String
+  format str _ = str <> symbolVal (Proxy @text)
 
-instance HasPrintf a => HasPrintf ((text :: Symbol) :<< a) where
+instance (KnownSymbol text, HasPrintf a)
+  => HasPrintf ((text :: Symbol) :<< a) where
   type Printf ((text :: Symbol) :<< a) = Printf a
+  format str _ = format (str <> symbolVal (Proxy @text)) (Proxy @a)
 
-instance HasPrintf a => HasPrintf ((param :: Type) :<< a) where
+instance (HasPrintf a, Show param) => HasPrintf ((param :: Type) :<< a) where
   type Printf ((param :: Type) :<< a) = param -> Printf a
+  format str _ = \param -> format (str <> show param) (Proxy @a)
