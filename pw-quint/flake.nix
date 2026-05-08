51c1ba14dbfce92942efdc499b6f28251f52f2f2
Damian Nadales <damian.only@gmail.com>
2026-05-08 13:30:12 -0300

    Add nix flake with Quint

---
+++ pw-quint/flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Quint development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = [
+          pkgs.quint
+        ];
+      };
+    };
+}
